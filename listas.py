8c3d1832984862935bdb55fe3ea44bb1730ce6b5
korlay37 <ercorlay7_@msn.com>
2025-01-14 23:56:31 -0500

    Added listas

--- listas.py
+++ listas.py
@@ -1,6 +1,52 @@
-mi_lista: list[str] = ['manzana', 'pera']
+mi_lista: list[str] = ["manzana", "pera", "cereza"]
 
-# append
+# append(elemento)
+# Agrega un elemento al final de la lista.
+mi_lista.append("coco")
+print(mi_lista)
+
+# insert(posicion, elemento)
+# Inserta un elemento en una posición específica.
+mi_lista.insert(2, "sandia")
+print(mi_lista)
+
+# extend(iterable)
+# Agrega todos los elementos de un iterable (como otra lista) al final de la lista.
+mi_lista.extend(["platano", "melon"])
+print(mi_lista)
+
+# remove(elemento)
+# Elimina la primera aparición de un elemento en la lista.
+mi_lista.remove("pera")
+print(mi_lista)
+
+# pop(posicion)
+# Elimina y devuelve el elemento en una posición específica. Si no se especifica, elimina el último
+elemento: str = mi_lista.pop(2)
+elemento: str = mi_lista.pop()
+
+# sort(key=None, reverse=False)
+# Ordena la lista en orden ascendente o descendente (en su lugar)
+mi_lista.sort()
+mi_lista.sort(key=lambda name: name.lower())
+mi_lista.sort(key=lambda name: len(name))
+
+# reverse()
+# Invierte el orden de los elementos de la lista. (en su lugar)
+mi_lista.reverse()
+
+# count(elemento)
+# Devuelve la cantidad de veces que un elemento aparece en la lista.
+print(mi_lista.count("coco"))
+
+# copy()
+# Devuelve una copia superficial de la lista.
+nueva_lista: list[str] = mi_lista.copy()
+
+# index(elemento, inicio=0, fin=None)
+# Devuelve el índice de la primera aparición del elemento. Opcionalmente, puedes buscar en un rango.
+print(mi_lista.index("sandia"))
+
 # clear
-# copy
-# remove
+# Elimina todos los elementos de la lista, dejándola vacía.
+mi_lista.clear()
